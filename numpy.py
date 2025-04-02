53e548254eae267217958ed103ba9d1165479803
Nipun Jayawardhana <120259186+nipun-jayawardhana@users.noreply.github.com>
2025-04-02 23:06:08 +0530

    import-numpy

---
+++ numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+a = np.array([10,20,30,40,50])
+b = np.array([2,3,4,5,6])
+
+c = a + b
+print(c)
+
+c = a - b
+print(c)
+
+c = a * b
+print(c)
+
+c = a / 2
+print(c)
+
+c = b ** 2
+print(c)
+
+print()
+#using max,min,sum and mean
+print(a.sum())
+print(a.max())
+print(a.min())
+print(a.mean())
