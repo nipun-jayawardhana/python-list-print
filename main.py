f85b0fe428a9a98a9489cf13603c7750cee98e60
Nipun Jayawardhana <120259186+nipun-jayawardhana@users.noreply.github.com>
2025-04-02 20:54:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+x=["apple","banana","berry",45,56.7,True]
+#first type how to print list data
+i=0
+while i < len(x):
+    print(x[i])
+    i=i+1
+
+print()
+
+#second type how to print list data
+for i in range (0,len(x)):
+    print(x[i])
+
+print()
+#third type how to print list data
+for item in x:
+    print(item)
